81dbb437b16819cd0d866e806d1485c0d1ebffac
nomadboy <39433626+nomadboy@users.noreply.github.com>
2018-07-31 10:56:19 +0600

    Add files via upload

---
+++ contact-form.php
@@ -0,0 +1,28 @@
+<?php 
+
+	$name = $_POST['name'];
+
+	$user_email = $_POST['email'];
+
+	$message = $_POST['message'];
+
+	$email_from = 'boynomad008@gmail.com';
+
+	$email_subject = 'New mail';
+
+	$email_body = "User name: $name.\n".
+					"User email: $user_email.\n".
+					"User message: $message.\n";
+
+	$to = 'lovebirdlovebird07890@gmail.com';
+
+	$header = "From: $email_from \r\n";
+
+	$header .= "Reply to: $user_email \r\n";
+
+
+	mail($to, $email_subject, $email_body, $header);
+
+	header("Location: index.php");
+
+ ?>
